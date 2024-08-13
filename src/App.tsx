235211948d7d2eb77f5a93f6c7b820d89e753473
unknown <dunjoyebiz2020@gmail.com>
2024-08-13 16:07:39 +0100

    Added React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Difficulty from './components/Difficulty/difficulty';
 
 
-//define the difficulty parameter for each tier
-//Easy will have 10 questions and 15 seconds to answer each
-//Medium will have 20 questions with 10 seconds to answer each
-//Hard will have 40 questions with 5 seconds to answer each
 
 function App() {
   return (
--- src/components/Difficulty/difficulty.tsx
+++ src/components/Difficulty/difficulty.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import Questions from "../Questions/questions";
+/* import Questions from "../Questions/questions"; */
+
+//define the difficulty parameter for each tier
+//Easy will have 10 questions and 15 seconds to answer each
+//Medium will have 20 questions with 10 seconds to answer each
+//Hard will have 40 questions with 5 seconds to answer each
+//put each defined difficulty in a button such that on click the questions are being rendered
+//I think react router will be very useful here
 
 const Difficulty: React.FC = () => {
-    return(
-        <>
-            <Questions 
-            numberOfQuestions={5}
-            durationForQuestions={10000}/>
-        </>
-    );
-}
+  return (
+    <>
+    <h1>Welcome to Trivia</h1>
+    <p>Choose your difficulty level</p>
+      <button>Easy</button>
+      <button>Medium</button>
+      <button>Hard</button>
+    </>
+  );
+};
 
 export default Difficulty;
